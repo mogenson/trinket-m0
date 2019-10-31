6c5a4266f889c04a3d74d4ace4f2b93cbb64dc92
Michael Mogenson <michael.mogenson@gmail.com>
2019-10-31 17:17:51 -0400

    atmel start files

---
+++ driver_init.c
@@ -0,0 +1,33 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_gclk_base.h>
+#include <hpl_pm_base.h>
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PA10
+
+	gpio_set_pin_level(LED,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(LED, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(LED, GPIO_PIN_FUNCTION_OFF);
+}
