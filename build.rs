1aadf57d2364cdd788821d4a0b0883b5f2566933
Michael Mogenson <michael.mogenson@gmail.com>
2019-11-01 08:41:09 -0400

    build static library from atmel-start

--- build.rs
+++ build.rs
@@ -3,6 +3,8 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
+extern crate cc;
+
 fn main() {
     // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
@@ -15,4 +17,43 @@ fn main() {
     // Only re-run the build script when memory.x is changed,
     // instead of when any part of the source code changes.
     println!("cargo:rerun-if-changed=memory.x");
+
+    // compile the atmel-start sources as a static library
+    cc::Build::new()
+        .define("__SAMD21E18A__", None)
+        .file("atmel-start/atmel_start.c")
+        .file("atmel-start/driver_init.c")
+        .file("atmel-start/examples/driver_examples.c")
+        .file("atmel-start/hal/src/hal_atomic.c")
+        .file("atmel-start/hal/src/hal_delay.c")
+        .file("atmel-start/hal/src/hal_gpio.c")
+        .file("atmel-start/hal/src/hal_init.c")
+        .file("atmel-start/hal/src/hal_io.c")
+        .file("atmel-start/hal/src/hal_sleep.c")
+        .file("atmel-start/hal/utils/src/utils_assert.c")
+        .file("atmel-start/hal/utils/src/utils_event.c")
+        .file("atmel-start/hal/utils/src/utils_list.c")
+        .file("atmel-start/hal/utils/src/utils_syscalls.c")
+        .file("atmel-start/hpl/core/hpl_core_m0plus_base.c")
+        .file("atmel-start/hpl/core/hpl_init.c")
+        .file("atmel-start/hpl/dmac/hpl_dmac.c")
+        .file("atmel-start/hpl/gclk/hpl_gclk.c")
+        .file("atmel-start/hpl/pm/hpl_pm.c")
+        .file("atmel-start/hpl/sysctrl/hpl_sysctrl.c")
+        .file("atmel-start/samd21a/gcc/gcc/startup_samd21.c")
+        .file("atmel-start/samd21a/gcc/system_samd21.c")
+        .include("atmel-start")
+        .include("atmel-start/CMSIS/Core/Include")
+        .include("atmel-start/config")
+        .include("atmel-start/hal/include")
+        .include("atmel-start/hal/utils/include")
+        .include("atmel-start/hpl/core")
+        .include("atmel-start/hpl/dmac")
+        .include("atmel-start/hpl/gclk")
+        .include("atmel-start/hpl/pm")
+        .include("atmel-start/hpl/port")
+        .include("atmel-start/hpl/sysctrl")
+        .include("atmel-start/hri")
+        .include("atmel-start/samd21a/include")
+        .compile("libhal.a");
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,31 @@
 
 // pick a panicking behavior
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
+// from: http://nercury.github.io/rust/embedded/experiments/2019/01/27/rust-embedded-02-measuring-the-clock.html
+extern crate cty;
+extern "C" {
+    fn atmel_start_init();
+    fn delay_ms(ms: cty::uint16_t);
+    fn gpio_toggle_pin_level(pin: cty::uint8_t);
+}
+
 #[entry]
 fn main() -> ! {
+    let led: u8 = 10;
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
 
+    unsafe {
+        atmel_start_init();
+    }
+
     loop {
-        // your code goes here
+        unsafe {
+            delay_ms(1000);
+            gpio_toggle_pin_level(led);
+        }
     }
 }
