a591ed94fa6dc04b485768ed5c9933e945f6e6f9
Michael Mogenson <michael.mogenson@gmail.com>
2019-11-21 09:35:04 -0500

    update dependencies
    remove cortex-m-semihosting

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,17 @@ extern crate panic_halt;
 mod hal;
 use cortex_m_rt::entry;
 use embedded_hal::blocking::delay::DelayMs;
-use embedded_hal::digital::v2::InputPin;
 use embedded_hal::digital::v2::OutputPin;
 
 #[entry]
 fn main() -> ! {
     hal::init();
-    let mut led = hal::Pin::new(hal::gpio_port_GPIO_PORTA, 10u8).into_output();
-    let input = hal::Pin::from(11u8).into_pull_down_input();
-    led.set_high().unwrap();
+    let mut led = hal::Pin::from(hal::PIN_PA10 as u8).into_output();
 
     loop {
         hal::Delay.delay_ms(1000u16);
-        hal::Delay.delay_ms(input.is_high().unwrap() as u8);
+        led.set_high().unwrap();
+        hal::Delay.delay_ms(500u16);
+        led.set_low().unwrap();
     }
 }
